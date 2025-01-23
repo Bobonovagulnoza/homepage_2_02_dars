561db890dbd09d7b51bc738646e222420a7682b2
Bobonovagulnoza <bobonovagulnoza68@gmail.com>
2025-01-23 20:04:39 +0500

    onboarding_qo'shildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,59 @@
-// import 'package:flutter/material.dart';
-// import 'detail_page/travel/presentation/pages/travel_main_page.dart';
-// import 'home_page/presentation/widgets/main_body_items.dart';
-// void main(){
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       // home: OnboardingView (),
-//       // home: MainBodyItems(),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'detail_page/core/client.dart';
-import 'detail_page/core/utils/sizes.dart';
-import 'detail_page/core/utils/theme.dart';
-import 'detail_page/package_detail/data/repositories/package_repository.dart';
-import 'detail_page/package_detail/presantation/pages/package_detail_view.dart';
-import 'detail_page/package_detail/presantation/pages/package_detail_view_model.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  runApp(const TravelApp());
+import 'onboarding/welcome_page_view.dart';
+
+void main(){
+  runApp(const MyApp());
 }
 
-class TravelApp extends StatelessWidget {
-  const TravelApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    AppSizes.init(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppThemes.lightTheme,
-      home: PackageDetailView(
-        viewModel: PackageDetailViewModel(
-          repo: PackageRepository(
-            client: ApiClient(),
-          ),
-        ),
-      ),
+      home: OnboardingView (),
+      // home: MainBodyItems(),
     );
   }
 }
 
 
-
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'detail_page/core/client.dart';
+// import 'detail_page/core/utils/sizes.dart';
+// import 'detail_page/core/utils/theme.dart';
+// import 'detail_page/package_detail/data/repositories/package_repository.dart';
+// import 'detail_page/package_detail/presantation/pages/package_detail_view.dart';
+// import 'detail_page/package_detail/presantation/pages/package_detail_view_model.dart';
+//
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+//   runApp(const TravelApp());
+// }
+//
+// class TravelApp extends StatelessWidget {
+//   const TravelApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     AppSizes.init(context);
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: AppThemes.lightTheme,
+//       home: PackageDetailView(
+//         viewModel: PackageDetailViewModel(
+//           repo: PackageRepository(
+//             client: ApiClient(),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+//
+//
--- lib/utils/AppColors.dart
+++ lib/utils/AppColors.dart
@@ -5,6 +5,7 @@ class AppColors{
   static const Color GrayBorder = Color(0xFF4B4B4B80);
   static const Color TextColor = Color(0xFF4B4B4B);
   static const Color BorderColor = Color(0xFF069C57);
+  // static const Color GreenMain = Color(0xFF3BB77E);
 
 
 

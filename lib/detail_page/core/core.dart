a44298535b5df6eb32e3c6b8a7fb1104a65a27b5
Bobonovagulnoza <bobonovagulnoza68@gmail.com>
2025-01-27 18:03:52 +0500

    sms kod kirtish qismi

--- lib/detail_page/core/core.dart
+++
@@ -1,9 +0,0 @@
-export 'client.dart';
-export 'presentation/widgets/bottom_nav_bar/bottom_nav_bar.dart';
-export 'presentation/widgets/bottom_nav_bar/bottom_nav_bar_item.dart';
-export 'presentation/widgets/tour_package/days_in_city.dart';
-export 'presentation/widgets/tour_package/tour_package_feature.dart';
-export 'utils/colors.dart';
-export 'utils/sizes.dart';
-export 'utils/styles.dart';
-export 'utils/theme.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
+
+import 'package:ever_travel_project/ruyxatdan_utish/presentation/pages/kod_kiritish_page.dart';
+import 'package:ever_travel_project/travel/presentation/pages/HomeView_page.dart';
+import 'package:ever_travel_project/travel/presentation/pages/MoreDetails_page.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-import 'home_page/presentation/widgets/main_body_items.dart';
-import 'onboarding/welcome_page_view.dart';
+import 'OnBoarding/presentation/pages/OnPoarding_page.dart';
+import 'SignUp/presentation/pages/EnterNumber_page.dart';
+import 'malumot_kiritish/presentation/pages/malumotlarni_kiritish_page.dart';
 
-void main(){
+GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/enter_the_code',
+      builder: (context, state) => KodKiritishPage(),
+    ),
+    GoRoute(
+      path: '/enter_number',
+      builder: (context, state) => EnterNumberPage(),
+    ),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => OnBoarding(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => HomeView(),
+    ),
+    GoRoute(
+      path: '/detail',
+      builder: (context, state) => MoreDetails(),
+    ),
+    GoRoute(
+      path: '/malumot_kiritish',
+      builder: (context, state) => MalumotlarniKiritishPage(),
+    ),
+  ],
+);
+
+void main() {
   runApp(const MyApp());
 }
 
@@ -12,70 +48,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      // home: OnboardingView (),
-      home: MainBodyItems(),
+      routerConfig: router,
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'detail_page/core/client.dart';
-// import 'detail_page/core/utils/sizes.dart';
-// import 'detail_page/core/utils/theme.dart';
-// import 'detail_page/package_detail/data/repositories/package_repository.dart';
-// import 'detail_page/package_detail/presantation/pages/package_detail_view.dart';
-// import 'detail_page/package_detail/presantation/pages/package_detail_view_model.dart';
-//
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-//   runApp(const TravelApp());
-// }
-//
-// class TravelApp extends StatelessWidget {
-//   const TravelApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     AppSizes.init(context);
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: AppThemes.lightTheme,
-//       home: PackageDetailView(
-//         viewModel: PackageDetailViewModel(
-//           repo: PackageRepository(
-//             client: ApiClient(),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-//
-//
